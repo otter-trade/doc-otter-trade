9f64cc69c9c6529d06c80aec29d620282a5f78ee
墨七 <mo7@mo7.cc>
2024-05-24 19:54:55 +0800

    创建docs项目

---
+++ _shell/init.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+## 环境变量
+function GitSet {
+  echo " ====== git设置大小写敏感,文件权限变更 ====== "
+  git config core.ignorecase false
+  git config --global core.fileMode false
+  git config core.filemode false
+  git config pull.rebase false
+  chmod -R 777 ./
+}
+
+# 项目根目录
+NowPath=$(pwd)
+
+# 最终的输出目录
+OutPutPath="${NowPath}/dist"
+
+# 远程仓库目录地址
+DeployPath="git@github.com:mo7Web/Mo7BlogPackage.git" # 待定
+
+# Deploy 完成之后的输出  待定
+DeployEndText="
+https://github.com/mo7Web/Mo7BlogPackage
+https://blog.mo7.cc
+"
+
+echo "
+===== 初始化变量 =====
+
+当前目录: ${NowPath}
+输出目录: ${OutPutPath}
+远程仓库目录: ${DeployPath}
+完成之后的输出: ${DeployEndText}
+
+"
