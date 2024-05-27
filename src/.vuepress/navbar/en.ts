036d8e6e68a4c08efbe0d7b5b38680f80d1a80e9
墨七 <mo7@mo7.cc>
2024-05-27 23:52:38 +0800

    x

--- src/.vuepress/navbar/en.ts
+++ src/.vuepress/navbar/en.ts
@@ -3,12 +3,7 @@ import { navbar } from 'vuepress-theme-hope';
 export const enNavbar = navbar([
   {
     text: 'OtterTrade.com',
-    icon: 'book',
-    link: 'https://OtterTrade.com',
-  },
-  {
-    text: 'OtterTrade.com',
-    icon: 'book',
+    icon: 'yumingwangzhan',
     link: 'https://OtterTrade.com',
   },
 ]);
