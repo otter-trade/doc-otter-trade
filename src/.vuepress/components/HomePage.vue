151d3e90672c1a9a47220a0f7c6605df7cd6f0a5
mo7 <mo7@mo7.cc>
2024-06-03 15:11:57 +0800

    x

---
+++ src/.vuepress/components/HomePage.vue
@@ -0,0 +1,28 @@
+<script setup lang="ts">
+import AppPackage from '../../../package.json';
+
+const textConsole = `   
+███╗   ███╗ ██████╗ ███████╗
+████╗ ████║██╔═══██╗╚════██║
+██╔████╔██║██║   ██║    ██╔╝
+██║╚██╔╝██║██║   ██║   ██╔╝ 
+██║ ╚═╝ ██║╚██████╔╝   ██║  
+╚═╝     ╚═╝ ╚═════╝    ╚═╝`;
+
+const PrintlnInfo = () => {
+  const PackageData: any = AppPackage;
+  const Text = `${textConsole}${PackageData.version}`;
+  console.log(`%c${Text}`, 'color: #eddfcd');
+  return Text;
+};
+</script>
+
+<template>
+  <div class="mo7-home-page">这里是首页内容</div>
+</template>
+
+<style lang="scss">
+.mo7-home-page {
+  background-color: red;
+}
+</style>
